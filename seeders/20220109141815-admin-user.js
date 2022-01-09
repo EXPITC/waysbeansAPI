2e696cc1f5ae5977cc4e1a2f5bf31587b6fdc70f
EXPITC <indratc18@gmail.com>
2022-01-09 21:53:46 +0700

    add sequelize-cli

--- seeders/20220109141815-admin-user.js
+++ seeders/20220109141815-admin-user.js
@@ -1,4 +1,5 @@
 'use strict';
+const { users } = require('../models')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -11,19 +12,27 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-      await queryInterface.bulkInsert(
-       'users',
-       [
-         {
-           email: 'owner@mail.com',
-           password: '$2b$08$4sDpJvJJ7msCddZ00n019.8O/ub9umIBe4wqivyDmGgDReAClyK9.8O/ub9umIBe4wqivy',  //123123
-           fullname: 'owner',
-           role: 'owner',
-           image: 'LOFI.jpg'
-         },
-       ],
-       {}
-     );
+     let valid = await users.findOne({
+      where: {email : 'owner@mail.com'}
+      })
+
+      if (valid) {
+        console.log('already have owner')  
+      } else {
+        await queryInterface.bulkInsert(
+         'users',
+         [
+           {
+             email: 'owner@mail.com',
+             password: '$2b$08$4sDpJvJJ7msCddZ00n019.8O/ub9umIBe4wqivyDmGgDReAClyK9.8O/ub9umIBe4wqivy',  //123123
+             fullname: 'owner',
+             role: 'owner',
+             image: 'LOFI.jpg'
+           },
+         ],
+         {}
+       );
+      }
   },
 
   down: async (queryInterface, Sequelize) => {
